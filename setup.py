d6c6251b153c4a3878afdd61938fc9239fb5539a
Robert Forkel <xrotwang@googlemail.com>
2021-03-08 14:04:09 +0100

    release 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyamsd',
-    version='0.1.0',
+    version='1.0.0',
     author='Hans-Jörg Bibiko',
     author_email='lingweb@shh.mpg.de',
     description='Python library for AMSD data curation',
