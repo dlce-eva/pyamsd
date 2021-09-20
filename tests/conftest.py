9bd551b3ac2f0368a0584a169387620b43de1cef
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2021-09-20 15:55:09 +0200

    made usage of shutil.copytree to eliminate deprecate warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,10 @@
 import pathlib
-
+import shutil
 import pytest
-
-from clldutils.path import copytree
 
 
 @pytest.fixture
 def repos(tmp_path):
     repos = tmp_path / 'amsd-data'
-    copytree(pathlib.Path(__file__).parent / 'repos', repos)
+    shutil.copytree(pathlib.Path(__file__).parent / 'repos', repos)
     return repos
