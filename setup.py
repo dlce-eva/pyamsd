e47d4f67ef39a1260bfb5865dd8e80efa255f333
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2021-09-20 15:51:16 +0200

    fixed cdstarcat version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'tqdm',
         'clldutils>=3.9.0',
-        'cdstarcat>=1.30',
+        'cdstarcat>=1.3.0',
         'attrs',
     ],
     extras_require={
